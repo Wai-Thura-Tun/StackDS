f195290420e79a172090acf4420f7365da7cc0c0
Thura <waithura13579@gmail.com>
2022-10-24 19:55:20 +0630

    Stack

--- main.cpp
+++ main.cpp
@@ -1,37 +1,40 @@
 #include "stdio.h"
 #include "stdlib.h"
-#define RangeValue -9595
-#define STACK 10
+#define RangeValue -9595;
+typedef struct node {
+    int data;
+    struct node* next;
+}Node, *NodePtr;
+
 typedef struct {
-    int top;
-    int sp[STACK];
+    NodePtr top;
 }StackType,*Stack;
 
 Stack initStack(){
     Stack sp = (Stack) malloc(sizeof(StackType));
-    sp->top = -1;
+    sp->top = NULL;
     return sp;
 }
 int empty(Stack s) {
-    return (s->top == -1);
+    return (s->top ==  NULL);
 }
 
 int pop(Stack s) {
     if (empty(s)) {
         return RangeValue;
     }
-    int hold = s->sp[s->top];
-    --s->top;
+    int hold = s->top->data;
+    NodePtr temp = s->top;
+    s->top = s->top->next;
+    free(temp);
     return hold;
 }
 
 void push(Stack s, int n) {
-    if (s->top > STACK - 1) {
-        printf("Stack overflow\n");
-        exit(1);
-    }
-    ++s->top;
-    s->sp[s->top] = n;
+    NodePtr node = (NodePtr) malloc(sizeof(Node));
+    node->data = n;
+    node->next = s->top;
+    s->top = node;
 }
 int main() {
     Stack initStack();
